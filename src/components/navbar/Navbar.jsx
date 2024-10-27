208bbb27af77bece6eabf0ddd0795509a89e487c
Harsh-Mangukia <harshmangukia123@gmail.com>
2024-10-28 00:00:48 +0530

    Added images

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { RiMenu3Line,RiCloseLine } from 'react-icons/ri'
 import './Navbar.css'
+import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='gpt3Navbar'>
+      <div className='gpt3NavbarLinks'>
+        <div className='gpt3NavbarLinksLogo'>
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
     </div>
   )
 }
