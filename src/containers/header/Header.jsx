3a94124794188b555f12c89b8d59c57bd042e301
Harsh-Mangukia <harshmangukia123@gmail.com>
2024-10-29 10:49:57 +0530

    Added html to Header.jsx and Some CSS

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="gpt3Header sectionPadding" id="home">
+      <div className="gpt3HeaderContent">
+        <h1 className="gradientText">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt3HeaderContentInput">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3HeaderContentPeople">
+          <img src={people} alt="people"/>
+          <p>1,600 people requested access a visit in 24 hours</p>
+        </div>
+        <div className="gpt3HeaderImage">
+          <img src={ai} alt='AI' />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
